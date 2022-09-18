0da3f24b5f80d1a33a4f3ea07387b0aa6396e729
theodore <616364596@qq.com>
2022-09-18 20:32:31 +0800

    20.Spring-Boot-Swagger2

---
+++ 20.Spring-Boot-Swagger2/src/test/java/com/xiaotu/swagger/SwggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaotu.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
