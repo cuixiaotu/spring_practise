68d26b4372efa9a59914a98e4a73928b8e7c9f02
theodore <616364596@qq.com>
2022-09-23 13:58:00 +0800

    21.Spring-Boot-Actuator 有问题

---
+++ 21.Spring-Boot-Actuator/src/test/java/com/xiaotu/actuator/ActuatorApplicationTests.java
@@ -0,0 +1,17 @@
+package com.xiaotu.actuator;
+
+import org.assertj.core.util.Maps;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Base64;
+import java.util.Map;
+
+@SpringBootTest
+class ActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
